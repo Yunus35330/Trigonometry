f0aa7d7915dbc0f60ae8dd76fde5b312b906aa6a
Unicon <yunuskizkapan@outlook.com>
2020-05-15 15:49:27 +0300

    version_02

--- main.c
+++ main.c
@@ -4,15 +4,15 @@ int main(){
 	float pi = 3.14159265359 ,x = 0.0;
 	int r;
 
-	printf("Dairenizin cevre uzunlugunu ogrenmek icin yaricap'ini giriniz(cm): \n");
+	printf("To find the perimeter of the circle, enter its radius(cm): \n");
 	scanf("%d" ,&r);
-	printf("Dairenizin cevre uzunlugu = %f cm'dir.\n\n" ,(2*pi)*r);
+	printf("Circumference of the circle: = %f cm.\n\n" ,(2*pi)*r);
 
-	printf("Radyan degeri icin 'aci derecesi' degerini giriniz:\n");
+	printf("Enter the 'angle degree' value for the radian value:\n");
 	scanf("%f" ,&x);
-	printf("acinizin gordugu yay uzunlugu = %f radyan'dir.\n\n" ,x*(pi/180));
+	printf("Arc length seen by angle = %f radians.\n\n" ,x*(pi/180));
 
-	printf("%1.f derece acinin yay uzunlugu = %f cm'kadar eder." ,x ,(2*pi*r)/360*x);
+	printf("The arc length of %1.f degree angle is about = %f cm." ,x ,(2*pi*r)/360*x);
 
     getch();
 return 0;
